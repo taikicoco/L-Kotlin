688378d10a1d0fb78c403bc701ad649c6f1d5ced
taikicoco <kawasakitaiki0308@gmail.com>
2024-03-17 13:20:34 +0900

    update

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,3 @@
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 fun main() {
     val name = "Kotlin"
     println("Hello, " + name + "!")
